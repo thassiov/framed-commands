928b1695257097fbb92c661c6cd423b81c2f75ed
thassiov <tvmcarvalho@gmail.com>
2021-06-04 09:35:17 -0300

    Add selection menu for $HOME/.tuizer dir scan

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,88 @@
 #!/usr/bin/env node
 
+/* eslint-disable-next-line */
+const inquirerSearchList = require('inquirer-search-list');
+
 import { resolve } from 'path';
+import inquirer from 'inquirer';
 
 import CommandRunner from './command-runner';
 import {IJSONConfigFile} from './definitions/IJSONConfigFile';
+import {directoryLoader} from './internal-tools/directoryLoader';
 import {fileLoader} from './internal-tools/fileLoader';
 import {JSONParser} from './internal-tools/JSONParser';
 import {logger} from './internal-tools/logger';
 import {processCliArgs} from './internal-tools/parseCliArgs';
 import {renderUi} from './ui';
 
+async function getHomeConfigs () {
+  if(!process.env.HOME) {
+    throw new Error('HOME env var is not set. Exiting');
+  }
+
+  const configsLocation = resolve(process.env.HOME, '.tuizer');
+
+  let content;
+  try {
+  content = await directoryLoader(configsLocation);
+  } catch (dirErr) {
+    if (dirErr.code == 'ENOENT') {
+      throw new Error('No $HOME/.tuizer directory found. Exiting');
+    }
+    throw dirErr;
+  }
+
+  if (!content.length) {
+    throw new Error('No files found at $HOME/.tuizer. Exiting ');
+  }
+
+  return content.map((config) => resolve(configsLocation, config));
+}
+
+function getUserInput () {
+  // gets argument (config file path) from the command line
+  const args = processCliArgs(process.argv);
+  return args[0];
+}
+
+async function runSelectionMenu(configs: string[]): Promise<string> {
+  return new Promise((resolve, reject) => {
+    inquirer.registerPrompt("search-list", inquirerSearchList);
+
+    inquirer
+    .prompt([
+      {
+        type: "search-list",
+        message: "Select config",
+        name: "config",
+        choices: configs,
+      }
+    ])
+    .then(function({ config }) {
+      console.log(config);
+      resolve(config);
+    })
+    .catch(e => reject(e));
+  });
+}
+
 (async () => {
   try {
-    // gets argument (config file path) from the command line
-    const args = processCliArgs(process.argv);
+
+    const cliInputFileLocation = getUserInput();
+
+    let filePath;
+
+    if (cliInputFileLocation) {
+      filePath = resolve(process.cwd(), cliInputFileLocation);
+    } else {
+      const configs = await getHomeConfigs();
+      const selected = await runSelectionMenu(configs);
+      filePath = selected;
+    }
 
     // loads the file based on the cwd (why cwd?)
-    const file = await fileLoader(resolve(process.cwd(), args[0] || ''));
+    const file = await fileLoader(filePath);
 
     // gets the 'commands' prop (an array) from the config file
     const { commands, name = '' } = JSONParser(file.toString()) as IJSONConfigFile;
