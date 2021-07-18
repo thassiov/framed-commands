9d36611a30a10b7f5cd611954b7503c8090f2c07
thassiov <tvmcarvalho@gmail.com>
2021-07-18 16:49:52 -0300

    Indicate with colors the state of the command

--- src/ui/components/command-status/CommandStatus.tsx
+++ src/ui/components/command-status/CommandStatus.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 
 import { Box, Text } from 'ink';
 import Spinner from 'ink-spinner';
@@ -13,6 +13,26 @@ type CommandStatusProps = {
 };
 
 const CommandStatus: FC<CommandStatusProps> = ({ commandData }: CommandStatusProps) => {
+  const [statusColor, setStatusColor] = useState('blue');
+
+  useEffect(() => {
+    switch (commandData.status) {
+      case CommandStatusEnum.RUNNING:
+        setStatusColor('green');
+        break;
+      case CommandStatusEnum.ERROR:
+        setStatusColor('red');
+        break;
+      case CommandStatusEnum.KILLED:
+      case CommandStatusEnum.STOPPED:
+        setStatusColor('orange');
+        break;
+      default:
+        setStatusColor('blue');
+        break;
+    }
+  });
+
   return (
     <Box
       justifyContent="space-between"
@@ -22,11 +42,8 @@ const CommandStatus: FC<CommandStatusProps> = ({ commandData }: CommandStatusPro
       </Box>
       <Box>
         <Text>Status: </Text>
-        <Text color={'blue'}>{ commandData.status.toUpperCase() }</Text>
-        {
-          commandData.status == CommandStatusEnum.RUNNING &&
-              <Spinner type={'point'} />
-        }
+        <Text color={statusColor}>{ commandData.status.toUpperCase() } </Text>
+        { commandData.status == CommandStatusEnum.RUNNING && <Spinner type={'point'} /> }
       </Box>
     </Box>
   );
