183d8ac6391d9aeff0fb372bac0641429d7f2f3f
thassiov <tvmcarvalho@gmail.com>
2024-01-08 18:16:41 -0300

    Add prettier

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
 
-import { renderUi } from './ui';
+import { renderUi } from './ui/';
 
 (() => {
+  // @TODO here we should initialize the application:
+  //  - load the database
+  //  - load the user frames (configs) inside the application directory
+  //  - load the services
+  //  - then call the ui renderer
   renderUi();
 })();
